49545230451c57b5db420aa975c99221ce961934
stev3j <mygajeong@gmail.com>
2022-07-12 14:17:09 +0900

    HandDetection

--- handdetection.py
+++ handdetection.py
@@ -1,29 +1,77 @@
 import cv2
 import mediapipe as mp
+import numpy as np
 
-cap = cv2.VideoCapture(0)
+def handdetection():
 
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
-mpDraw = mp.solutions.drawing_utils
+    max_num_hands = 1
+    gesture = {0:'zero', 1:'one', 2:'two', 3:'three', 4:'four', 5:'five',
+                6:'six', 7:'seven', 8:'eight', 9:'nine'}
 
-while True:
-    ret, frame = cap.read()
-    imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) #표현할 이미지의 색
-    results = hands.process(imgRGB) #이미지 전처리
-    print(results.multi_hand_landmarks)
+    # MediaPipe hands model
+    mp_hands = mp.solutions.hands
+    mp_drawing = mp.solutions.drawing_utils
+    hands = mp_hands.Hands(
+        max_num_hands=max_num_hands,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5)
 
-    #인식이 됐다면
-    if results.multi_hand_landmarks:
-        for handLms in results.multi_hand_landmarks: #각 점의 xyz좌표
-            mpDraw.draw_landmarks(frame, handLms, mpHands.HAND_CONNECTIONS) #각 점 찍어주기
+    # Gesture recognition model
+    file = np.genfromtxt('gesture_train.csv', delimiter=',')
+    angle = file[:,:-1].astype(np.float32)
+    label = file[:, -1].astype(np.float32)
+    knn = cv2.ml.KNearest_create()
+    knn.train(angle, cv2.ml.ROW_SAMPLE, label)
 
+    cap = cv2.VideoCapture(0)
 
-    #읽어온 프레임 출력
-    cv2.imshow('frame', frame)
-    #나가기
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
+    while cap.isOpened():
+        ret, img = cap.read()
+        if not ret:
+            continue
 
-cap.release()
-cv2.destroyAllWindows() 
+        img = cv2.flip(img, 1)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        result = hands.process(img)
+
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+        if result.multi_hand_landmarks is not None:
+            for res in result.multi_hand_landmarks:
+                joint = np.zeros((21, 3))
+                for j, lm in enumerate(res.landmark):
+                    joint[j] = [lm.x, lm.y, lm.z]
+
+                # Compute angles between joints
+                v1 = joint[[0,1,2,3,0,5,6,7,0,9,10,11,0,13,14,15,0,17,18,19],:] # Parent joint
+                v2 = joint[[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],:] # Child joint
+                v = v2 - v1 # [20,3]
+                # Normalize v
+                v = v / np.linalg.norm(v, axis=1)[:, np.newaxis]
+
+                # Get angle using arcos of dot product
+                angle = np.arccos(np.einsum('nt,nt->n',
+                    v[[0,1,2,4,5,6,8,9,10,12,13,14,16,17,18],:], 
+                    v[[1,2,3,5,6,7,9,10,11,13,14,15,17,18,19],:])) # [15,]
+
+                angle = np.degrees(angle) # Convert radian to degree
+
+                # Inference gesture
+                data = np.array([angle], dtype=np.float32)
+                ret, results, neighbours, dist = knn.findNearest(data, 3)
+                idx = int(results[0][0])
+
+                # Draw gesture result
+                if idx in gesture.keys():
+                    return gesture[idx].upper()
+                    cv2.putText(img, text=gesture[idx].upper(), org=(int(res.landmark[0].x * img.shape[1]), int(res.landmark[0].y * img.shape[0] + 20)), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
+
+                mp_drawing.draw_landmarks(img, res, mp_hands.HAND_CONNECTIONS)
+
+        cv2.imshow('Game', img)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+a = handdetection()
+print(a)
